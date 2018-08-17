a86e9603d53fd5fec9488a76fd48222fb89fb39b
tanakaworld <yutaro.tanaka.world@gmail.com>
2018-08-17 10:54:41 +0900

    Show PDF from base64 string

---
+++ src/components/pdf/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="PDF">
+        <button @click="onClickOpenPDF">Open PDF</button>
+    </div>
+</template>
+
+<script>
+    import pdfLight from './raw/pdf-light.txt';
+
+    const generatePDFFileUrl = (base64String) => {
+        const file = new Blob([atob(base64String)], {type: 'application/pdf'});
+        const fileURL = URL.createObjectURL(file);
+        // eslint-disable-next-line
+        console.log(fileURL);
+        return fileURL;
+    };
+
+    export default {
+        name: "PDF",
+        methods: {
+            onClickOpenPDF() {
+                window.open(generatePDFFileUrl(pdfLight));
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    configureWebpack: {
+        module: {
+            rules: [
+                {
+                    test: /\.txt$/,
+                    use: 'raw-loader'
+                }
+            ]
+        }
+    }
+};
